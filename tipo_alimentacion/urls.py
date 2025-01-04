3f718b825835c77373bac29fc8aa134c7aefe106
Luis Pizano <luispizano883@gmail.com>
2025-01-04 16:47:02 -0300

    Se añade funcionalidades eliminar y editar a listar tipo alimentacion

--- tipo_alimentacion/urls.py
+++ tipo_alimentacion/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('tipo_alimentación/',views.agregar_tipo, name = 'agregar_alimentacion'),
     path('listar_tipo_alimentación/',views.listar_tipo_alimentacion, name = 'listar_alimentacion'),
+    path('eliminar_tipo_alimentación/<int:id>',views.eliminar_tipo_alimentacion, name = 'eliminar_tipo_alimentacion'),
+    path('editar_tipo_alimentación/<str:nombre>',views.editar_tipo_alimentacion, name = 'editar_tipo_alimentacion'),
 ]
--- tipo_alimentacion/views.py
+++ tipo_alimentacion/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib import messages
 from .models import TipoAlimentacion
 from .forms import TipoAlimentacionForm
@@ -19,3 +19,29 @@ def agregar_tipo(request):
 def listar_tipo_alimentacion(request):
     tipos = TipoAlimentacion.objects.all()
     return render(request,'listar_tipos_alimentacion.html',{'tipos':tipos})
+
+def eliminar_tipo_alimentacion(request,id):
+    tipo = get_object_or_404(TipoAlimentacion,id_tipo_alimentacion=id)
+    if request.method =='POST':
+        tipo.delete()
+        messages.success(request,"Tipo de alimentación eliminada exitosamente.")
+        return redirect('listar_alimentacion')
+    else:
+        messages.error(request,"Ocurrio un error al intentar eliminar el tipo de alimentación.")
+    return render(request,'listar_tipos_alimentacion.html')
+
+def editar_tipo_alimentacion(request, nombre):
+    tipo = get_object_or_404(TipoAlimentacion, nombre_tipo_alimentacion=nombre)
+    
+    if request.method == 'POST':
+        form = TipoAlimentacionForm(request.POST, instance=tipo)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Tipo de alimentación actualizada exitosamente.")
+            return redirect('listar_alimentacion')
+        else:
+            messages.error(request, 'Ocurrió un error al intentar editar el tipo de alimentación.')
+    else:
+        form = TipoAlimentacionForm(instance=tipo)
+    
+    return render(request, 'editar_tipo_alimentacion.html', {'form': form})
