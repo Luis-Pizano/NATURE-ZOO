d7394850832f64174a4cc46130f371d7f4090b2f
Luis Pizano <luispizano883@gmail.com>
2025-01-06 17:14:56 -0300

    Se agrega imagen a listar_especies

--- especies/urls.py
+++ especies/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
 
+from django.conf.urls.static import static
+from nature_zoo import settings
+
 urlpatterns = [
     path('agregar_especies/',views.agregar_especies, name = 'agregar_especies'),
     path('listar_especies/',views.listar_especies, name = 'listar_especies'),
     path('eliminar_especie/<int:id>',views.eliminar_especie, name = 'eliminar_especie'),
     path('editar_especie/<str:nombre>',views.editar_especie, name = 'editar_especie'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
